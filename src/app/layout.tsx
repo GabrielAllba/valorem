c3bc5b964bc72dab8e898fd9738240b5688685f7
rielgbrl <rielgasy9@gmail.com>
2023-11-09 04:26:08 +0700

    add icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,9 @@ import './globals.css';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-    title: '...',
-    description: '...',
+    title: 'Valorem | Valuasi Kekayaan Intelektual',
+    description: 'Solusi mudah, aman dan terpercaya untuk valuasi kekayaan intelektual Anda',
+    icons: 'valorem.jpeg',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
